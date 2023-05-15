d2b2f88bda0cbedc77e6b1732510f6ad1fc41294
CK Aznable <kami25565@gmail.com>
2023-05-15 21:21:56 +0800

    perf: multiple thread task

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use clap::Parser;
-use ringbuf::LocalRb;
+use ringbuf::{Consumer, LocalRb, SharedRb};
 use speech::{SpeechConfig, WhisperPayload};
 use std::{
     error::Error,
     ffi::c_int,
     io::{BufRead, BufReader},
+    mem::MaybeUninit,
     process::{Child, ChildStdout, Command, Stdio},
-    time::{Duration, Instant},
+    sync::{
+        mpsc::{self, Receiver},
+        Arc,
+    },
+    thread::{self, JoinHandle},
+    time::Instant,
 };
-use wait_timeout::ChildExt;
 use whisper_rs::WhisperContext;
 
 use util::Log;
@@ -17,6 +22,13 @@ mod audio;
 mod speech;
 mod util;
 mod vad;
+
+type U8Consumer = Consumer<u8, Arc<SharedRb<u8, Vec<MaybeUninit<u8>>>>>;
+
+enum ThreadState {
+    End,
+    Sync,
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -44,65 +56,15 @@ struct Args {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let logger = Log::new(args.verbose);
+    let (tx, rx) = mpsc::channel::<ThreadState>();
 
     // 1Mb buffer
     let rb_size = 1024 * 1024;
     let mut rb = LocalRb::<u8, Vec<_>>::new(rb_size);
     let (mut ts_prod, mut ts_cons) = rb.split_ref();
 
-    let ctx = WhisperContext::new(&args.model).expect("failed to load model");
-    let mut state = ctx.create_state().expect("failed to create state");
-
-    let mut last_processed = Instant::now();
-    let mut streaming_time = 0.0f64;
-    let collect_time = Duration::from_secs(5);
-
     let (mut child, stdout) = get_yt_dlp_stdout(&args.url);
     let mut reader = BufReader::new(stdout);
-
-    let mut process = || {
-        if ts_cons.is_empty() {
-            logger.error(audio::Error::Empty.to_string());
-            return;
-        }
-
-        let data = ts_cons.pop_iter().collect::<Vec<u8>>();
-        logger.verbose(format!("Reading {}kb data from yt-dlp", data.len() / 1024));
-
-        match audio::get_audio_data(&data) {
-            Ok((audio_data, dur)) => {
-                logger.verbose(format!(
-                    "Get {}kb audio data and duration {:.3}s from ts",
-                    audio_data.len() / 1024,
-                    dur
-                ));
-
-                let config = SpeechConfig::new(args.threads as c_int, Some(&args.lang));
-                let mut payload: WhisperPayload = WhisperPayload::new(&audio_data, config);
-                let running_calc = Instant::now();
-
-                let segment_time = (streaming_time * 1000.0) as i64;
-                streaming_time += dur;
-
-                speech::process(&mut state, &mut payload, &mut |segment, start| {
-                    println!(
-                        "[{}] {}",
-                        util::format_timestamp_to_time(segment_time + start),
-                        segment
-                    );
-                });
-
-                logger.verbose(format!(
-                    "whisper process time: {}s",
-                    running_calc.elapsed().as_secs()
-                ));
-            }
-            Err(err) => {
-                logger.error(err.to_string());
-            }
-        }
-    };
 
     loop {
         let buf = reader.fill_buf()?;
@@ -113,18 +75,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         let len = buf.len();
         ts_prod.push_slice(buf);
 
-        if last_processed.elapsed() >= collect_time || ts_prod.is_full() {
-            process();
-            last_processed = Instant::now();
+        if ts_prod.is_full() {
+            todo!();
         }
 
         reader.consume(len);
     }
 
-    process();
-    child
-        .wait_timeout(Duration::from_secs(3))
-        .expect("failed to wait on yt-dlp");
+    child.kill().expect("failed to wait on yt-dlp");
+    let _ = tx.send(ThreadState::End);
 
     Ok(())
 }
@@ -144,4 +103,61 @@ fn get_yt_dlp_stdout(url: &str) -> (Child, ChildStdout) {
     let stdout = child.stdout.take().expect("invalid stdout stream");
 
     (child, stdout)
+}
+
+fn evoke_whisper_thread(
+    args: Args,
+    rx: Receiver<ThreadState>,
+    mut cons: U8Consumer,
+) -> JoinHandle<()> {
+    let ctx = WhisperContext::new(&args.model).expect("failed to load model");
+    let logger = Log::new(args.verbose);
+
+    thread::spawn(move || {
+        let mut state = ctx.create_state().expect("failed to create state");
+        let mut streaming_time = 0.0f64;
+
+        while let Ok(ThreadState::Sync) = rx.recv() {
+            if cons.is_empty() {
+                logger.error(audio::Error::Empty.to_string());
+                continue;
+            }
+
+            let data = cons.pop_iter().collect::<Vec<u8>>();
+            logger.verbose(format!("Reading {}kb data from yt-dlp", data.len() / 1024));
+
+            match audio::get_audio_data(&data) {
+                Ok((audio_data, dur)) => {
+                    logger.verbose(format!(
+                        "Get {}kb audio data and duration {:.3}s from ts",
+                        audio_data.len() / 1024,
+                        dur
+                    ));
+
+                    let config = SpeechConfig::new(args.threads as c_int, Some(&args.lang));
+                    let mut payload: WhisperPayload = WhisperPayload::new(&audio_data, config);
+                    let running_calc = Instant::now();
+
+                    let segment_time = (streaming_time * 1000.0) as i64;
+                    streaming_time += dur;
+
+                    speech::process(&mut state, &mut payload, &mut |segment, start| {
+                        println!(
+                            "[{}] {}",
+                            util::format_timestamp_to_time(segment_time + start),
+                            segment
+                        );
+                    });
+
+                    logger.verbose(format!(
+                        "whisper process time: {}s",
+                        running_calc.elapsed().as_secs()
+                    ));
+                }
+                Err(err) => {
+                    logger.error(err.to_string());
+                }
+            }
+        }
+    })
 }
